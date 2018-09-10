243c8d6071c25ff907d68986fe3be98faba85412
avdglm <arvidgilljam@gmail.com>
2018-09-10 19:07:03 +0200

    email update 2

--- php/contact.php
+++ php/contact.php
@@ -43,7 +43,7 @@
 	$email = $_POST['email'];
 	$message = $_POST['message'];
 	$from = $email;
-	$to = 'hej@matteappen.com';  // please change this email id
+	$to = 'arvid@matteappen.com';  // please change this email id
 	$subject = 'Contact Form : Titan - The best downloaded template ever';
 
 	$body = "From: $name\n E-Mail: $email\n Message:\n $message";
